37c56340e04ea2d0ea645db323b077ebc4362773
Juan J Rebollo <jj.rebollo.barranco@gmail.com>
2023-03-08 19:05:39 +0000

    FinnhubClient project in place

---
+++ FinnhubClient/FinnhubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinnhubClientApp.swift
+//  FinnhubClient
+//
+//  Created by Juan Jose Rebollo on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinnhubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
